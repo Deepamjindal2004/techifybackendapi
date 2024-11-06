c29562611913e454b2e6a66109a9e8737cd07d00
Muskaansaini22 <sainimuskaan022@gmail.com>
2024-11-06 13:38:27 -0500

    Debugging the app.js

--- app.js
+++ app.js
@@ -6,19 +6,20 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config(); // To access environment variables
 
-const userRoutes = require('./routes/userRoutes');
-const adRoutes = require('./routes/adRoutes');
-
-// Use routes
-app.use('/api/users', userRoutes);
-app.use('/api/ads', adRoutes);
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+const userRoutes = require('./routes/userRoutes');
+const adRoutes = require('./routes/adRoutes');
+
+// Use routes
+app.use('/api/users', userRoutes);
+app.use('/api/ads', adRoutes);
+
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
@@ -36,5 +37,5 @@ app.use((err, req, res, next) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(Server is running on port ${PORT});
 });
