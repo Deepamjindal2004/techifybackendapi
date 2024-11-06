1ea8578d75cb6a682d9ae84e0601e8219541dfbf
Deepamjindal2004 <jindaldeepam@gmail.com>
2024-11-06 12:58:15 -0500

    Creating Controllers to Handle Business Logic

---
+++ controllers/userController.js
@@ -0,0 +1,35 @@
+// controllers/userController.js
+
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+    const { username, password, email } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashedPassword, email });
+
+    try {
+        const savedUser = await user.save();
+        res.status(201).json({ user: savedUser._id });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+exports.login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username });
+        if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) return res.status(400).json({ message: 'Invalid credentials' });
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ token });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
