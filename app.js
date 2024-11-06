91652d3e7f800b28efd0966efe84085d87ec02fb
sudhanshu234432 <123100827+sudhanshu234432@users.noreply.github.com>
2024-11-06 13:42:22 -0500

    debugging app.js

--- app.js
+++ app.js
@@ -37,5 +37,5 @@ app.use((err, req, res, next) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(Server is running on port ${PORT});
+    console.log(`Server is running on port ${PORT}`);
 });
