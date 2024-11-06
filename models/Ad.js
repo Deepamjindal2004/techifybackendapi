9d2d8c6019455a36a18d6eeac82e18fe81ce9f47
vaibhavkalia9585 <vaibhavkalia9585@gmail.com>
2024-11-06 12:54:00 -0500

    Creating Models for MongoDB

---
+++ models/Ad.js
@@ -0,0 +1,14 @@
+// models/Ad.js
+
+const mongoose = require('mongoose');
+
+const adSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    startDate: { type: Date, default: Date.now },
+    endDate: { type: Date, required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Ad', adSchema);
